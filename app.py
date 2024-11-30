91973ea5a7260862f09be9ff5c30998ad305f306
rhy <rhynomatty100@gmail.com>
2024-11-30 04:03:14 -0300

    Clean up app.py

--- app.py
+++ app.py
@@ -1,29 +1,10 @@
 from flask import Flask, g, request, render_template, url_for, make_response
 
 app = Flask(__name__)
-files = {
-	"logo": "images/logo.png",
-	"pink_star": "images/icons/pink_star.png"
-}
-
-### Functions
-
-def read(path: str) -> str:
-	with open(path, "r") as file:
-		return file.read()
-
-def render_main_page(name: str):
-	return render_template("main.html.j2", title=name, content=read(f"static/main_pages/{name}.html"))
 
 @app.before_request
 def before_request():
 	g.load_static = lambda path: url_for("static", filename=path)
-
-### App Routes
-
-# @app.route("/")
-# def index():
-# 	return read("static/placeholder.html")
 
 @app.route("/", defaults={"name": "placeholder"})
 @app.route("/about/", defaults={"name": "about"})
@@ -34,20 +15,8 @@ def before_request():
 @app.route("/resources/", defaults={"name": "resources"})
 @app.route("/other_sites/", defaults={"name": "other_sites"})
 def main_page(name):
-	return render_main_page(name)
-
-# I tried and tried and couldn't find a better way than this. Maybe one day.
-# @app.route("/about/")
-# def about():
-# 	return render_main_page("about")
-
-# @app.route("/credits/")
-# def credits():
-# 	return render_main_page("credits")
-
-# @app.route("/links/")
-# def links():
-# 	return render_main_page("links")
+	with open(f"static/main_pages/{name}.html", "r") as file:
+		return render_template("main.html.j2", title=name, content=file.read())
 
 @app.errorhandler(404)
 def not_found(_):
